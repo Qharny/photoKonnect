53159d1be6e22aa01611dd846900a49d4832606c
qharny <kabuteymanasseh5@gmail.com>
2025-06-20 19:25:29 +0000

    added dependecies

---
+++ lib/app_color.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color primaryBlue = Color(0xFF326BFF);
+  static const Color accentPurple = Color(0xFF7F56D9);
+
+  // Secondary Colors
+  static const Color softSkyBlue = Color(0xFFEAF2FF);
+  static const Color lightLavender = Color(0xFFF3ECFF);
+
+  // Neutral Colors
+  static const Color darkText = Color(0xFF333333);
+  static const Color mediumGray = Color(0xFF888888);
+  static const Color lightGray = Color(0xFFE0E0E0);
+  static const Color offWhite = Color(0xFFFAFAFA);
+
+  // Feedback Colors
+  static const Color successGreen = Color(0xFF28C76F);
+  static const Color warningYellow = Color(0xFFFFC107);
+  static const Color errorRed = Color(0xFFEA5455);
+}
